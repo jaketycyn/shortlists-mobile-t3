8b15f0c35c63b1d81895f1a7fbbbf1cf9ff554c2
Jake TyCyn <jaketycyn@gmail.com>
2022-11-16 09:48:32 -0800

    CR: Constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,1 @@
-export let baseUrl = process.env.VERCEL
-
-
-() => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-    return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-  };
+export const baseUrl = process.env.VERCEL ? `https://${process.env.VERCEL}` : 'http://localhost:3000'
