4d867309d434516a1a475f9e9311b3a4e2dfc057
Jake TyCyn <jaketycyn@gmail.com>
2022-11-16 09:48:23 -0800

    UP: schema changed to extend

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,17 +1,29 @@
 import { request } from 'http'
-import z from 'zod'
+import z, { string } from 'zod'
 
-export const createUserSchema = z.object({
-    username: z.string(),
-    email: z.string().email(),
-    password: z.string(),
+export const userLoginSchema = z.object({
+    email: string().email(),
+    password: z.string().min(5).max(15),
 })
 
-export type CreateUserInput = z.TypeOf<typeof createUserSchema>
+export const userRegisterSchema = userLoginSchema.extend({
+    username: z.string(),
+})
+
 
 export const requestOTPSchema = z.object({
     email: z.string().email(),
     redirect: z.string().default('/')
 })
 
+
+
+export const verifyOTPSchema = z.object({
+    hash: z.string(),
+})
+
+export type userLogin = z.TypeOf<typeof userLoginSchema>
+export type userRegister = z.TypeOf<typeof userRegisterSchema>
+
 export type requestOTPInput = z.TypeOf<typeof requestOTPSchema>
+export type verifyOTPInput = z.TypeOf<typeof verifyOTPSchema>
